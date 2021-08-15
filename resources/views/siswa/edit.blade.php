dc19c0408cff0cb16309cfa8bea49121e44355c2
Ajengcp <ajengcp@webmail.umm.ac.id>
2021-08-16 02:52:46 +0700

    program page

---
+++ resources/views/siswa/edit.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.master')
+@section('title', 'Edit  ')
+@section('content')
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Siswa</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('siswa.index') }}">Siswa</a></li>
+                    <li class="breadcrumb-item active">Edit</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Edit Data</h3>
+            </div>
+            <!-- /.card-header -->
+            <form action="{{ route('siswa.update', [$siswa->id]) }}" method="POST" enctype="multipart/form-data">
+                @CSRF
+                @method('put')
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="siswaImage">Foto</label>
+                                <input type="file" name="siswaImage" class="form-control @error('siswaImage') is-invalid @enderror" value="{{$siswa->siswaImage}}">
+                                <img src="{{ asset('images/siswa/'.$siswa->siswaImage) }}" width="100px" height="80px" alt="Image">
+                                <small class="text-danger">@error('siswaImage') {{$message}} @enderror</small>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="siswaDesc">Deskripsi</label>
+                                <textarea name="siswaDesc" class="form-control @error('siswaDesc') is-invalid @enderror" value="{{$siswa->siswaDesc}}">{{$siswa->siswaDesc}}</textarea>
+                                <small class="text-danger">@error('siswaDesc') {{$message}} @enderror</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+                <div class="card-footer">
+                    <div class="d-flex justify-content-end">
+                        <a href="{{ route('siswa.index') }}" class="m-1 btn btn-outline-danger">Back</a>
+                        <button type="submit" class="m-1 btn btn-success">Update</button>
+                    </div>
+                </div>
+            </form>
+        </div><!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+@endsection
